7cf22ccce9a699c8ab59c363a9cd6369a18f9952
Pam <pamela.glownia@gmail.com>
2021-11-19 22:24:18 +0100

    Use ExecutorService to run threads

--- src/main/java/pl/glownia/pamela/Main.java
+++ src/main/java/pl/glownia/pamela/Main.java
@@ -1,16 +1,18 @@
 package pl.glownia.pamela;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main {
     public static void main(String[] args) {
         Controller controller = new Controller();
         Blockchain blockchain = new Blockchain();
         Command blockCreating = new BlockCreator(blockchain);
-
-        for (int i = 0; i < 5; i++) {
-            Thread minerThread = new Thread(new MinerThread(controller, blockchain, blockCreating));
-            minerThread.start();
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        for (int i = 0; i <= 10; i++) {
+            executorService.execute(new MinerThread(controller, blockchain, blockCreating));
             try {
-                minerThread.join();
+                Thread.sleep(1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
